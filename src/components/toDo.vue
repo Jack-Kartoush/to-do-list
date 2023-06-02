999f30b90f9f4a26f82924ab385822c60cfd77ef
Jack Kartoush <jackkartosh@gmail.com>
2023-06-02 10:31:43 +0200

    adding a run.sh file

--- src/components/toDo.vue
+++ src/components/toDo.vue
@@ -1,32 +1,27 @@
-
 <template>
-    <div class="warning"> 
+  <div class="warning">
     <span>You have to fil the filed</span>
-    </div>
-  </template>
+  </div>
+</template>
 
-  <script>
-   export default{
-    name: 'toDo'
-   }
+<script>
+export default {
+  name: "toDo",
+};
 </script>
-  <style>
-    .warning {
-     
-     display: flex;
-     justify-content: center;
-     align-items: center;
-     margin-bottom: 10px;
-    
-    }
-    .warning span{
-      text-align: center;
-      padding: 15px;
-      border-radius: 40px 0 20px 0;
-      background-color: rgb(200, 8, 8);
-      color:white;
-      font-weight: bold;
-    }
-  </style>
- 
-
+<style>
+.warning {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.warning span {
+  text-align: center;
+  padding: 15px;
+  border-radius: 40px 0 20px 0;
+  background-color: rgb(200, 8, 8);
+  color: white;
+  font-weight: bold;
+}
+</style>
